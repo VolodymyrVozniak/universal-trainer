c61f8000c0f6d13c48fc23f9623d606eb6edee37
Volodymyr Vozniak <croatoan@MacBook-Air-Volodymyr.local>
2023-01-28 21:03:53 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name='croatoan-trainer',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/VolodymyrVozniak/universal-trainer',
+    license='',
+    author='Volodymyr Vozniak',
+    author_email='vozniak.v.z@gmail.com',
+    description='Universal trainer and visualizator',
+    install_requires=[
+        'torch>=1.13.0',
+        'scikit-learn>=1.2.0',
+        'pandas>=1.5.2',
+        'plotly>=5.7.0'
+    ],
+    python_requires=">=3.8",
+)
