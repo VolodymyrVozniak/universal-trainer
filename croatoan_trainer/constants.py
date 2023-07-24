339fc47a5c88746e552c756bd790f79e3f83fd3e
VolodymyrVozniak <vovo4ka9@gmail.com>
2023-07-24 13:19:22 +0300

    change LOG_FILENAME

--- croatoan_trainer/constants.py
+++ croatoan_trainer/constants.py
@@ -8,6 +8,6 @@ torch.set_float32_matmul_precision('high')
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 SEED = 785372
 
-LOG_FILENAME = "logs_2.log"
+LOG_FILENAME = "logs.log"
 LOG_MODE = "w"
 LOGGER = logging.getLogger(__name__)
